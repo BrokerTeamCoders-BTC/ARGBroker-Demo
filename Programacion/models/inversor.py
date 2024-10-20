134e9816ff1079d9447c081fdcbbb4036e4c30fa
Corval <alejandromcorva@gmail.com>
2024-10-19 22:45:00 -0300

    Clase inversor

--- Programacion/models/inversor.py
+++ Programacion/models/inversor.py
@@ -1,1 +1,8 @@
-# Clase Inversor.
+class Inversor:
+    def __init__(self, id_inversor, nombre, apellido, cuil, correo, contrasenia):
+        self.id_inversor = id_inversor
+        self.nombre = nombre
+        self.apellido = apellido
+        self.cuil = cuil
+        self.correo = correo
+        self.contrasenia = contrasenia 
