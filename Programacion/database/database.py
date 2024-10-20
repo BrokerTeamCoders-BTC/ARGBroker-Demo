9bcb6267e7b809b4396a156be55162782047fb2e
rodrichc <rodrichc1@gmail.com>
2024-10-20 18:34:02 -0300

    conexion a base de datos

--- Programacion/database/database.py
+++ Programacion/database/database.py
@@ -1,1 +1,19 @@
 # Conexión a base de datos
+import mysql.connector
+
+class ConexionBaseDeDatos:
+    def __init__(self):
+        self.conexion = None
+
+    def conectar(self):
+        self.conexion = mysql.connector.connect(
+            host='bi201yksz8ubhfjv7dyb-mysql.services.clever-cloud.com',
+            user='uihujxs4xmhkdvi3',
+            password='tp5pvzvCEPOye6QVmYAQ',
+            database='bi201yksz8ubhfjv7dyb',
+            port='3306'
+        )
+
+    def desconectar(self):
+        if self.conexion:
+            self.conexion.close()
