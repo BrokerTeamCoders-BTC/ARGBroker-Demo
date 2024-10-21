51150a3baee8adb5be188f1e37e44a1c52eed132
Corval <alejandromcorva@gmail.com>
2024-10-21 17:04:58 -0300

    Clase inversor encapsulamiento

--- Programacion/models/inversor.py
+++ Programacion/models/inversor.py
@@ -1,8 +1,27 @@
 class Inversor:
     def __init__(self, id_inversor, nombre, apellido, cuil, correo, contrasenia):
-        self.id_inversor = id_inversor
-        self.nombre = nombre
-        self.apellido = apellido
-        self.cuil = cuil
-        self.correo = correo
-        self.contrasenia = contrasenia 
+        self.__id_inversor = id_inversor
+        self.__nombre = nombre
+        self.__apellido = apellido
+        self.__cuil = cuil
+        self.__correo = correo
+        self.__contrasenia = contrasenia 
+
+    def get_id_inversor(self):
+        return self.__id_inversor
+   
+    def get_nombre(self):
+        return self.__nombre
+   
+    def get_apellido(self):
+        return self.__apellido
+   
+    def get_cuil(self):
+        return self.__cuil
+   
+    def get_correo(self):
+        return self.__correo
+   
+    def get_contrasenia(self):
+        return self.__contrasenia
+    
