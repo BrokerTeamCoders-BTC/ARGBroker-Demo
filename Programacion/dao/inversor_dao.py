391e0a5d4d6e7a35766e065a37e8eff0f804fb50
rodrichc <rodrichc1@gmail.com>
2024-10-21 21:31:53 -0300

    avance en programa principal y correccion de errores

--- Programacion/dao/inversor_dao.py
+++ Programacion/dao/inversor_dao.py
@@ -7,22 +7,22 @@ class InversorDAO:
 
     def crear_inversor(self, inversor):
         try:
-            with self.db.connection.cursor() as cursor:
+            with self.db.conexion.cursor() as cursor:
                 sql = """INSERT INTO Inversor (nombre, apellido, cuil, correo, contrasenia)
                         VALUES (%s, %s, %s, %s, %s)"""
                 values = (inversor.get_nombre(), inversor.get_apellido(), inversor.get_cuil(), inversor.get_correo(), inversor.get_contrasenia())
                 cursor.execute(sql, values)
                 id_inversor = cursor.lastrowid #Devuelve el último valor autoincremental generado (id_inversor)
-                self.db.connection.commit()
+                self.db.conexion.commit()
             return id_inversor
         except Exception as e:
             print(f"Error al crear el inversor: {e}")
-            self.db.connection.rollback()
+            self.db.conexion.rollback()
             return None
 
     def obtener_inversor_por_correo_y_contrasenia(self, correo, contrasenia):
         try:
-            with self.db.connection.cursor() as cursor:
+            with self.db.conexion.cursor() as cursor:
                 sql = "SELECT * FROM Inversor WHERE correo = %s and contrasenia = %s"
                 cursor.execute(sql, (correo, contrasenia))
                 inversor = cursor.fetchone()
--- Programacion/main.py
+++ Programacion/main.py
@@ -1,1 +1,74 @@
-# Programa de consola
+from database.database import ConexionBaseDeDatos
+from services.usuario_service import UsuarioService
+from utils.validacion_inversor import validar_datos
+
+def mostrar_menu_principal():
+    print("\n1. Ver datos de la cuenta")
+    print("2. Listar activos del portafolio")
+    print("3. Comprar acciones")
+    print("4. Vender acciones")
+    print("5. Cerrar sesión")
+
+def main():
+    db = ConexionBaseDeDatos()
+    db.conectar()
+
+    usuario_service = UsuarioService(db)
+    
+
+    print("\n------------ BIENVENIDO A ARGBROKER ------------")
+
+    while True:
+        print("\n1. Registrarse")
+        print("2. Iniciar sesión")
+        print("3. Salir")
+        opcion = input("\nSeleccione una opción: ")
+
+        if opcion == "1":
+            datos = validar_datos()
+            usuario_service.registrar_inversor(*datos)
+            print("\nInversor creado correctamente. Inicie sesión para operar.")
+
+        elif opcion == "2":
+            correo = input("\nCorreo: ")
+            contrasenia = input("Contraseña: ")
+            inversor = usuario_service.iniciar_sesion(correo, contrasenia)
+            
+            if inversor:
+                print(f"\n\n¡Bienvenido, {inversor.get_nombre()}!\n\n")
+                
+                
+                while True:
+                    mostrar_menu_principal()
+                    opcion_menu = input("\nSeleccione una opción: ")
+                    
+                    if opcion_menu == "1":
+                        pass
+
+                    elif opcion_menu == "2":
+                        pass
+                    
+                    elif opcion_menu == "3":
+                        pass
+                    
+                    elif opcion_menu == "4":
+                        pass
+
+                    elif opcion_menu == "5":
+                        break
+
+                    else: 
+                        print("Seleccione una opción correcta.")
+            else:
+                print("\n¡El correo o contraseña incorrectos!.")
+
+        elif opcion == "3":
+            break
+
+        else:
+            print("Seleccione una opción correcta")
+
+    db.desconectar()
+
+if __name__ == "__main__":
+    main()
