6371de857e84373247193f1df55155bba7878fc7
Corval <alejandromcorva@gmail.com>
2024-10-22 16:24:26 -0300

    Metodo obtener accion

--- Programacion/dao/accion_dao.py
+++ Programacion/dao/accion_dao.py
@@ -1,1 +1,20 @@
-# Todo lo que tenga que ver con una acción e involucre conexión a base de datos.
+from models.accion import Accion
+
+
+class AccionDAO:
+    def __init__(self, db_conexion):
+        self.db = db_conexion
+
+
+    def obtener_accion(self, id_accion):
+        try:
+            with self.db.conexion.cursor() as cursor:
+                sql = "SELECT * FROM Accion WHERE id_accion = %s"
+                cursor.execute(sql, (id_accion,))
+                accion = cursor.fetchone()
+            if accion:
+                return Accion(*accion)
+            return None
+        except Exception as e:
+            print(f"Error al obtener la acción: {e}")
+            return None
