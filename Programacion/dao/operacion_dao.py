306868e5f3a7c3f7b10522e3d07bf3e0141e9d39
rodrichc <rodrichc1@gmail.com>
2024-10-23 18:48:27 -0300

    registro de operacion en base de datos

--- Programacion/dao/operacion_dao.py
+++ Programacion/dao/operacion_dao.py
@@ -1,1 +1,15 @@
-# # Todo lo que tenga que ver con una operación e involucre conexión a base de datos.
+class OperacionDAO:
+    def __init__(self, db_conexion):
+        self.db = db_conexion
+
+    def registrar_operacion(self, id_portafolio, id_tipo, id_accion, fecha_operacion, precio, cantidad, total_accion, comision):
+        try:
+            with self.db.connection.cursor() as cursor:
+                sql = """INSERT INTO Operacion (id_portafolio, id_tipo, id_accion, fecha_operacion, precio, cantidad, total_accion, comision)
+                        VALUES (%s, %s, %s, %s, %s, %s, %s, %s)"""
+                values = (id_portafolio, id_tipo, id_accion, fecha_operacion, precio, cantidad, total_accion, comision)
+                cursor.execute(sql, values)
+                self.db.connection.commit()
+        except Exception as e:
+            print(f"Error al registrar operación: {e}")
+            self.db.connection.rollback()
